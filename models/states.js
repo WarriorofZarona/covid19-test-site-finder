687c57835f53e3ad4eee0561f80b06b3be6b4464
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-31 19:20:03 -0400

    added code models for cities and states

---
+++ models/states.js
@@ -0,0 +1,26 @@
+module.exports = function (sequelize, DataTypes) {
+    const State = sequelize.define("State", {
+        // Giving the Author model a name of type STRING
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 2]
+            }
+        }
+    });
+
+    State.associate = function (models) {
+        State.hasMany(models.Site, {
+            onDelete: "cascade"
+        });
+    };
+
+    State.associate = function (models) {
+        State.hasMany(models.City, {
+            onDelete: "cascade"
+        });
+    };
+
+    return State;
+};
