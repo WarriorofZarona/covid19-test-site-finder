e185c20f9b2d45915725062a560623a63d8b2e4a
Andrew Bautista <andrewbautista85@gmail.com>
2020-04-02 19:40:17 -0400

    added include code for joins, working on switch statement dependent on the state being called for

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,8 +4,30 @@ module.exports = app => {
 
     app.get("/api", (req, res) => {
 
-        db.Site.findAll()
+        db.Site.findAll({
+            include: [db.State, db.City]
+        })
             .then(dbSite => res.json(dbSite))
+
+    });
+
+    app.get("/api/:state", (req, res) => {
+
+        let state;
+
+        switch (req.params.state) {
+            case "new-york":
+                state = NY;
+                break;
+            case "new-jersey":
+                state = NJ;
+                break;
+            case "pennsylvania":
+                state = PA;
+                break;
+            default:
+                throw err;
+        }
 
     })
 
