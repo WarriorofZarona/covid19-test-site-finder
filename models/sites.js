8020502314c518f69e105662e34c9a1bfc0732f2
Andrew Bautista <andrewbautista85@gmail.com>
2020-04-04 11:11:16 -0400

    added paranoid true to sites.js

--- models/sites.js
+++ models/sites.js
@@ -44,9 +44,12 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false,
             len: [1]
 
+        },
+    },
+        {
+            paranoid: true
         }
-
-    });
+    );
 
     Site.associate = function (models) {
         Site.belongsTo(models.City, {
