e894391a199f854c9ce7e662b72b58550f9bf0a4
Madel Liquido <madel.liquido@gmail.com>
2020-04-08 19:18:14 -0400

    Need to figure out how to display nj JSON data onto nj page

--- public/js/nj.js
+++ public/js/nj.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     // This file just does a GET request to get testing locations
     // and updates the HTML on the page
-    $.get("api/cities/new-jersey").then(function(data) {
-        $(".testingLocations").text(data.name);
+    $.get("/api/cities/new-jersey").then(function(data) {
+        $(".testingLocations").text(data);
+        console.log(data);
     });
 });
--- public/js/ny.js
+++ public/js/ny.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // This file just does a GET request to get testing locations
     // and updates the HTML on the page
-    $.get("api/cities/new-york").then(function(data) {
-        $(".testingLocations").text(data.name);
+    $.get("/api/cities/new-york").then(function(data) {
+        $(".testingLocations").text(data);
     });
 });
