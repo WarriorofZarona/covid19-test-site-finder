84719dd3aaaf2fd7cd473546a7de56e5767effc6
Madel Liquido <madel.liquido@gmail.com>
2020-04-09 15:11:16 -0400

    Sending form response to the backend

--- public/js/submissions.js
+++ public/js/submissions.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
 
     $.get("/cities/new-jersey").then(function(data) {
 
-        $("#source").change(function() {
+        $(".source").change(function() {
 
             var el = $(this);
 
             if (el.val() === "New Jersey") {
-                $("#status").empty()
+                $(".status").empty()
                 for (let i = 0; i < data.length; i++) {
-                    $('#status').append('<option>' + data[i].name + '</option>');
+                    $('.status').append('<option>' + data[i].name + '</option>');
                 };
             } else if (el.val() === "New York") {
-                $("#status").empty()
-                $("#status").append("  <option>Albany</option>  <option>Buffalo</option>  <option>Ithaca</option> <option>New York City</option>");
+                $(".status").empty()
+                $(".status").append("  <option>Albany</option>  <option>Buffalo</option>  <option>Ithaca</option> <option>New York City</option>");
             } else if (el.val() === "Pennsylvania") {
-                $("#status").empty()
-                $("#status").append("  <option>Aliquippa</option> <option>Philadelphia</option> <option>Pittsburgh</option> ");
+                $(".status").empty()
+                $(".status").append("  <option>Aliquippa</option> <option>Philadelphia</option> <option>Pittsburgh</option> ");
             }
         });
 
@@ -28,12 +28,34 @@ $(document).ready(function() {
 
 function submitRequest() {
     $(".submissionForm").serialize() // returns all the data in your form
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var address = $("address").val();
+    var state = $("#state").val();
+    var city = $("#city").val();
+    var open = $("#open").val();
+    var close = $("#close").val();
+    var walkIn = $("#walkin").val();
+    var driveThru = $("#drivethru").val()
+    var isHospital = $("hospital").val()
+    var qualifications = $("qualifications").val()
     $.ajax({
         type: "POST",
         url: '/api/sites',
-        data: $(".submissionForm").serialize(),
+        data: {
+            name: name,
+            address: address,
+            phone: phone,
+            walkIn: walkIn,
+            driveThru: driveThru,
+            isHospital: isHospital,
+            hoursOfOp: open + " to " + close,
+            qualifications: qualifications,
+            city: city,
+        },
         success: function() {
             console.log("success");
+            console.log();
         }
     });
 }
