389846170a0730b0ece35f3cc5f57af24baf9763
Madel Liquido <madel.liquido@gmail.com>
2020-04-09 11:47:41 -0400

    Got dependent scroll down to work for submissions.html and submissions.js using the city and state

---
+++ public/js/submissions.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+    $.get("/cities/new-jersey").then(function(data) {
+
+        $("#source").change(function() {
+
+            var el = $(this);
+
+            if (el.val() === "New Jersey") {
+                $("#status").empty()
+                for (let i = 0; i < data.length; i++) {
+                    $('#status').append('<option>' + data[i].name + '</option>');
+                };
+            } else if (el.val() === "New York") {
+                $("#status").empty()
+                $("#status").append("  <option>Albany</option>  <option>Buffalo</option>  <option>Ithaca</option> <option>New York City</option>");
+            } else if (el.val() === "Pennsylvania") {
+                $("#status").empty()
+                $("#status").append("  <option>Aliquippa</option> <option>Philadelphia</option> <option>Pittsburgh</option> ");
+            }
+        });
+
+    });
+
+
+
+});
